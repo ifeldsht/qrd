545153a7e60ee5d166b868767ecdef06299888eb
Ubuntu <ubuntu@ip-172-31-51-80.ec2.internal>
2017-03-01 11:19:11 +0000

    added mongodb

--- master.sh
+++ master.sh
@@ -23,4 +23,7 @@ unzip stanford-corenlp-full-2016-10-31.zip
 mv stanford-corenlp-full-2016-10-31 corenlp
 rm stanford-corenlp-full-2016-10-31.zip
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get install -y mongodb
 
