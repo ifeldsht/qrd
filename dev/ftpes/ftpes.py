b9c48e19f16b0a07c52a91b39cc44f50a2bd2f1f
Ubuntu <ubuntu@ip-172-31-51-80.ec2.internal>
2017-03-14 00:41:32 +0000

    login/pwd/file

--- dev/ftpes/ftpes.py
+++ dev/ftpes/ftpes.py
@@ -2,14 +2,12 @@ from ftplib import FTP_TLS
 import sys
 
 ftps = FTP_TLS('209.99.16.94')
-ftps.login('usr', 'pwd')  
+ftps.login(sys.argv[1], sys.argv[2])  
 ftps.prot_p()  
 ftps.retrlines('LIST')
 
-ftps.storbinary('STOR aaa.txt', open('ftpes.py'))
-#filename = 'remote_filename.bin'
-#print 'Opening local file ' + filename
-#myfile = open(filename, 'wb')
-#ftps.retrbinary('RETR %s' % filename, myfile.write)
+#ftps.storbinary('STOR aaa.txt', open('ftpes.py'))
+with open(sys.argv[3],"wb") as f:
+    ftps.retrbinary('RETR ' + sys.argv[3], f.write)
 
 ftps.close()
