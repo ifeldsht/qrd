13a45c9f4b740bbbc9217b2d200e1eb31eda3d97
Ubuntu <ubuntu@ip-172-31-51-80.ec2.internal>
2017-03-16 10:32:50 +0000

    changes in stoplist

--- dev/aesop_lda/aesop_lda.py
+++ dev/aesop_lda/aesop_lda.py
@@ -3,7 +3,7 @@ import logging
 import gensim
 
 def clean_list(txt):
-    stoplist = set('for a of the and to in or by not as so this that'.split())
+    stoplist = set('for a an of the and to in or by not as so this that their them with but with at on he his she her have had has him be is was were are it i will you they me do did your am there would when what where could my if'.split())
     return [w for w in txt.split() if w not in stoplist]
 
 def alloc(n,lda):
@@ -13,13 +13,13 @@ def alloc(n,lda):
     return x
 
 if __name__=="__main__":
-#    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
-#                        level=logging.INFO, filename='lda.log',)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+                        level=logging.INFO, filename='lda.log',)
 
     with open("/home/ubuntu/data/read_gov_aesop/all.txt",'r') as f:
         ndocs = f.read().split("\n")
         ndocs = [ d.split("#") for d in ndocs if len(d.strip())>0 ]
-        docs  = [ d[1] for d in ndocs ]
+        docs  = [ d[1].lower() for d in ndocs ]
         nd    = [ d[0] for d in ndocs ]
 
     texts = [clean_list(d) for d in docs]
@@ -27,13 +27,13 @@ if __name__=="__main__":
     corpus = [dictionary.doc2bow(t) for t in texts]
     lda = gensim.models.ldamodel.LdaModel(corpus=corpus, \
                                           id2word=dictionary,\
-       num_topics=10, update_every=1, chunksize=10000, passes=100)
+       num_topics=10, update_every=1, passes=100)
 
 #    with open('texts_corpus.json','w') as f:
 #         json.dump({"texts":texts,"corpus":corpus},f)
 #    dictionary.save('dictionary.gensim')
 #    lda.save('lda10.gensim')
-#    lda.print_topics(10)
+    lda.print_topics(10)
     r = [ alloc(10,lda[t]) for t in corpus ]
     res = { d[0]:d[1] for d in zip(nd,r)}
     
