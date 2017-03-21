308ec9528c5b79f136a63c88024657127616e4a8
Ubuntu <ubuntu@ip-172-31-51-80.ec2.internal>
2017-03-21 10:46:34 +0000

    saving results

--- dev/data_extraction/clinicaltrials_gov/ct_crawler.sh
+++ dev/data_extraction/clinicaltrials_gov/ct_crawler.sh
@@ -10,21 +10,41 @@ do
       cut -b 1-6 --complement           | \
       while read trial_url
   do
+    out_str=\"trial_url\":\"$trial_url\",
+    
     desc=$(lynx -dump $trial_url | \
       sed 's/^ *//g'             | \
       sed '/^Detailed Description:$/,/^Eligibility$/{//!b};d')
+
+    out_str=$out_str\"description\":\"$desc\",
+
     purp=$(lynx -dump $trial_url | \
       sed 's/^ *//g'             | \
       sed '/^Purpose$/,/^\[.*\]Condition$/{//!b};d')
-    echo "======================================" >> a.txt
+
+    out_str=$out_str\"purpose\":\"$purp\"
+
+    counter=0
+    abs_str="\"abstracts\":["
     lynx -listonly -dump $trial_url | \
       grep "/ct2/bye"               | \
       cut -b 1-6 --complement       | \
       while read url_abs
       do
+        #echo $url_abs
         abst=$(lynx -dump $url_abs  | \
-          sed '/^Abstract$/,/^KEYWORDS:$/{//!b};d')
-        echo $abst 
+          sed 's/^[ ]*//g'          | \
+          sed '/^Abstract$/,/^PMID:$/{//!b};d')
+        if ((counter>0))
+        then
+          abs_str=$abs_str","
+        fi
+        ((counter++))
+        abs_str=$abs_str"{\"abstract_url\":\""$url_abs"\","
+        abs_str=$abs_str"\"abstract\":\""$abst"\"}"
       done
+      abs_str=$abs_str"]"
+      echo "{"$out_str $abs_str"}" > a.txt
+      exit
   done
 done
