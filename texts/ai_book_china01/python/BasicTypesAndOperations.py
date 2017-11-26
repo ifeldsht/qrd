1aeddadab225ccdd9ab774c1d81e02d400949f6a
Ilya Feldshteyn <ilya@ilya-feldshteyns-imac.local>
2017-11-26 07:40:11 -0500

    Sample code

---
+++ texts/ai_book_china01/python/BasicTypesAndOperations.py
@@ -0,0 +1,33 @@
+# Bolean
+b = True
+
+# Integer number
+x = 2
+
+# Float number
+y = 1.23
+
+# String
+s = "Abc"
+
+# List of numbers:
+L = [1,2,3,4,5]
+
+# print everything:
+print b, x, y, s, L
+
+# Operations:
+b2 = not b
+print b, b2
+
+x2 = x * 31
+print x, x2
+
+y2 = y*y
+print y, y2
+
+s2 = s.lower()
+print s, s2
+
+L2 = L[::-1]
+print L, L2
---
+++ texts/ai_book_china01/python/TreeDescription.py
@@ -0,0 +1,19 @@
+Tree1 = ("Math classroom",[])
+Tree2 = ("Library",[])
+Tree3 = ("Chemical lab",[])
+Tree4 = ("Library",[])
+Tree5 = ("Restroom",[])
+Tree6 = ("2nd floor",[Tree4,Tree5])
+Tree7 = ("3rd floor",[Tree1,Tree2,Tree3])
+Tree8 = ("Right stair",[Tree6,Tree7])
+Tree9 = ("Basement",[])
+Tree10 = ("Gym",[])
+Tree11 = ("Left stair",[Tree9,Tree10])
+Tree12 = ("School",[Tree8,Tree11])
+Tree13 = ("Ticket office",[])
+Tree14 = ("Cinema",[Tree13])
+Tree15 = ("Street",[Tree12,Tree14])
+ 
+print Tree15
+
+
