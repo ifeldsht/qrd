35ff5a87c59164c5d6611c7a8cf4c2e57acea094
Ilya Feldshteyn <ilya@ilya-feldshteyns-imac.local>
2017-12-25 15:19:18 -0500

    Finalized search, examples of modules

---
+++ texts/ai_book_china01/python/Modules1.py
@@ -0,0 +1,7 @@
+import module1.func1
+import module1.func2
+
+print module1.func1.f1(2)
+print module1.func1.f2(7)
+print module1.func2.f1(3)
+print module1.func2.g(234)
---
+++ texts/ai_book_china01/python/Modules2.py
@@ -0,0 +1,6 @@
+from module1 import *
+
+print func1.f1(2)
+print func1.f2(7)
+print func2.f1(3)
+print func2.g(234)
